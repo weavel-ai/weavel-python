61873ba9cfff695d281a2b981652841ce872e966
engineerA314 <engineerA314@github.com>
2024-02-08 14:53:39 +0900

    feat: add track_user, remove save_metadata, update version 0.1.0

--- weavel/_constants.py
+++ weavel/_constants.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-if os.getenv("TESTING") == "true":
+if os.getenv("WEAVEL_TESTING") == "true":
     BACKEND_SERVER_URL = "http://localhost:8000"
 else:
     BACKEND_SERVER_URL = "https://unravel.up.railway.app"
--- weavel/types.py
+++ weavel/types.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, validator
-from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
+from typing import Any, Dict, List, Optional, Sequence, Tuple, Union, Literal
 from enum import Enum
 from uuid import UUID
 from datetime import datetime, date
@@ -26,24 +26,35 @@ class DataType(str, Enum):
     user_message = "user_message"
     assistant_message = "assistant_message"
     inner_step = "inner_step"
-    retrieved_content = "retrieved_content"
+    # retrieved_content = "retrieved_content"
 
 class BackgroundTaskType(str, Enum):
     start_trace = "start_trace"
     log_trace_data = "log_trace_data"
-    save_metadata_trace = "save_metadata_trace"
-    save_metadata_trace_data = "save_metadata_trace_data"
+    track_user = "track_user"
+    # save_metadata_trace = "save_metadata_trace"
     create_tags_trace_data = "create_tags_trace_data"
     
 class StartTraceBody(WeavelObject):
     """Start Trace body."""
+    type: Literal['StartTraceBody'] = 'StartTraceBody'
     user_uuid: str
     trace_uuid: str
     timestamp: Optional[str] = None
     metadata: Optional[Dict[str, str]] = None
+    
+class TrackUserBody(WeavelObject):
+    """Save metadata body."""
+    type: Literal['TrackUserBody'] = 'TrackUserBody'
+    user_uuid: str
+    event_name: str
+    properties: Dict[str, str]
+    timestamp: Optional[str] = None
 
 class SaveTraceDataBody(WeavelObject):
     """Log body."""
+    type: Literal['SaveTraceDataBody'] = 'SaveTraceDataBody'
+    user_uuid: str
     trace_uuid: str
     data_type: str
     data_content: str
@@ -51,22 +62,16 @@ class SaveTraceDataBody(WeavelObject):
     unit_name: Optional[str] = None
     metadata: Optional[Dict[str, str]] = None
     
-class SaveMetadataTraceBody(WeavelObject):
-    """Save metadata body."""
-    trace_uuid: str
-    metadata: Dict[str, str]
+# class SaveMetadataTraceBody(WeavelObject):
+#     """Save metadata body."""
+#     trace_uuid: str
+#     metadata: Dict[str, str]
 
-class SaveMetadataTraceDataBody(WeavelObject):
-    """Save trace_data metadata body."""
-    trace_uuid: str
-    data_type: str
-    data_content: str
-    metadata: Dict[str, str]
-    
 class WeavelRequest(WeavelObject):
     """Weavel Request."""
     task: str
-    body: Union[StartTraceBody, SaveTraceDataBody, SaveMetadataTraceBody, SaveMetadataTraceDataBody]
+    # body: Union[StartTraceBody, SaveTraceDataBody, SaveMetadataTraceBody, TrackUserBody]
+    body: Union[StartTraceBody, SaveTraceDataBody, TrackUserBody]
     
     @validator('task', pre=True)
     def validate_task(cls, v):
